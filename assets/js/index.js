60e7f1d8e9420e838593b19c2ff472769093fc93
Hedita <hedi.takrami@gmail.com>
2022-11-27 20:20:14 +0330

    add error handling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,25 @@
 const addIcon = document.getElementById("add-icon");
 const input = document.getElementById("input");
+const errorHandling = document.getElementById("error-handling")
 
 async function postTodo() {
   const url = "http://localhost:3030/todos";
-
-  const response = await fetch(url,{
-    method: "POST",
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({text: input.value})
-  });
+  
+  try {
+    const response = await fetch(url,{
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({text: input.value})
+    });
+    if (response.status != 200) {
+      errorHandling.innerText = "Something went wrong!";
+    }
+  }
+  catch (error) {
+    errorHandling.innerText = "Something went wrong!";
+  }
 }
 
 addIcon.addEventListener("click", function() {

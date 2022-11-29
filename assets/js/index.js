36805c6cd239460736a8e7ecf85f215ce4380538
Hedita <hedi.takrami@gmail.com>
2022-11-29 11:11:48 +0330

    change the ids name and fix the code format

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,34 +1,33 @@
-const addTaskIcon = document.getElementById("add-icon");
-const addTaskInput = document.getElementById("input");
-const errorMessage = document.getElementById("error-message")
+const addTaskIcon = document.getElementById("add-task-icon");
+const addTaskInput = document.getElementById("add-task-input");
+const errorMessage = document.getElementById("error-message");
 const backendErrorMessage = document.getElementById("backend-error-message");
 
 async function postTodo(todoText) {
   const url = "http://localhost:3030/todos";
-  
+
   try {
-    const response = await fetch(url,{
+    const response = await fetch(url, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({text: todoText})
+      body: JSON.stringify({ text: todoText }),
     });
     if (response.status === 400) {
       errorMessage.innerText = "Something went wrong!";
     }
-  }
-  catch(error) {
+  } catch (error) {
     backendErrorMessage.innerText = error.message;
   }
 }
 
-addTaskIcon.addEventListener("click", function() {
+addTaskIcon.addEventListener("click", function () {
   postTodo(addTaskInput.value);
   clearAddTodoInput();
-})
+});
 
-input.addEventListener("keydown",(e) => {
+input.addEventListener("keydown", (e) => {
   if (e.key == "Enter") {
     postTodo(addTaskInput.value);
     clearAddTodoInput();

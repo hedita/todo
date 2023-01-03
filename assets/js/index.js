4f677d333f1db82d2ee644386d457869a4f05949
hedita <84747080+hedita@users.noreply.github.com>
2023-01-03 11:41:31 +0330

    Update assets/js/index.js
    Co-authored-by: Siamak <siamak.aghaeipour@gmail.com>

--- assets/js/index.js
+++ assets/js/index.js
@@ -113,7 +113,7 @@ function showTasks(tasks) {
   todoList.innerHTML = sortTasksByStatus(sortTasksByTime(tasks))
     .map(({ text, createdAt, id: taskId, isDone, updatedAt }) =>
     `<li id="${taskId}" class="todo-item">
-    <input id="checkbox-${taskId}" class="checkbox" type="checkbox" ${isChecked(isDone)}/>
+    <input id="checkbox-${taskId}" class="checkbox" type="checkbox" ${isChecked(isDone)} />
     <div id="todo-container-${taskId}">
     <p class="todo" title="${formatDate(createdAt)} ${formatDate(updatedAt)}">${text}</p>
     </div>
